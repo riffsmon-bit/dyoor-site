c41ff18cca59c413e1fb72ac196e4283e2ecb43c
Brandon Duke <brandonduke@Brandons-MacBook-Air.local>
2026-06-03 21:52:39 -0400

    Expand face mask builder rules

--- src/config/dyoorBuilderRules.js
+++ src/config/dyoorBuilderRules.js
@@ -31,125 +31,129 @@
 //   ],
 //   message: "These eyes only work with selected hats or no hat."
 // }
+
+const SKI_MASKS = [
+  "Black Shystie.png",
+  "Pink Shystie.png"
+];
+
+const FACE_MASKS = [
+  ...SKI_MASKS,
+  "BOB Mask.png"
+];
+
+const LASER_EYES = [
+  "Abyss Laser.png",
+  "Lazy Laser.png",
+  "Radiation Glow.png",
+  "Radiation Glow (Red).png",
+  "Radiation Glow (Red)Laser.png"
+];
+
+const EYEWEAR = [
+  "Build Anything Glasses.png",
+  "Eye Patch.png",
+  "Monad Specs Black.png",
+  "Monad Specs Gold.png",
+  "Neverland Specs.png",
+  "Ricky V.png",
+  "Sunglasses.png",
+  "Tri-Specs.png",
+  "VR Headset.png"
+];
+
+const FACE_COVERING_MOUTHS = [
+  "Bandana Black.png",
+  "Bandana Pink.png"
+];
+
+const PROTRUDING_MOUTH_PROPS = [
+  "Cigar Mouth.png",
+  "Displeased toothpick.png",
+  "Gold Bar.png",
+  "Joint Mouth.png",
+  "Meh Cigarette.png",
+  "Party Horn Mouth.png",
+  "Pipe.png"
+];
+
+const TEE_SHIRTS = [
+  "Baby Blue Tee.png",
+  "Black N White Tee.png",
+  "Black N White Tee 2.png",
+  "Black Tee.png",
+  "Black Tee 2.png",
+  "Grey Tee.png",
+  "Kuru Tee.png",
+  "Torn Black Tee.png",
+  "Torn Tee.png",
+  "Torn Tee 2.png",
+  "White Tee.png",
+  "White Tee 2.png"
+];
+
+function incompatibleWithMany({ category, files, withCategory, withFiles, message }) {
+  return files.flatMap((file) => withFiles.map((withFile) => ({
+    type: "incompatible",
+    trait: { category, file },
+    with: { category: withCategory, file: withFile },
+    message
+  })));
+}
+
 export const DYOOR_BUILDER_RULES = [
   {
     type: "disabled",
     trait: { category: "Mouth", file: "Bendette.png" },
     message: "This mouth trait is disabled for the public builder."
   },
-  ...[
-    "Abyss Laser.png",
-    "Lazy Laser.png",
-    "Radiation Glow.png",
-    "Radiation Glow (Red).png",
-    "Radiation Glow (Red)Laser.png"
-  ].flatMap((eyeFile) => [
-    {
-      type: "incompatible",
-      trait: { category: "Eyes", file: eyeFile },
-      with: { category: "Hat", file: "Black Shystie.png" },
-      message: "Laser eyes do not work with ski masks."
-    },
-    {
-      type: "incompatible",
-      trait: { category: "Eyes", file: eyeFile },
-      with: { category: "Hat", file: "Pink Shystie.png" },
-      message: "Laser eyes do not work with ski masks."
-    }
-  ]),
-  ...[
-    "Build Anything Glasses.png",
-    "Monad Specs Black.png",
-    "Monad Specs Gold.png",
-    "Neverland Specs.png",
-    "Ricky V.png",
-    "Sunglasses.png",
-    "Tri-Specs.png",
-    "VR Headset.png"
-  ].flatMap((eyeFile) => [
-    {
-      type: "incompatible",
-      trait: { category: "Eyes", file: eyeFile },
-      with: { category: "Hat", file: "Black Shystie.png" },
-      message: "Glasses and specs do not work with ski masks."
-    },
-    {
-      type: "incompatible",
-      trait: { category: "Eyes", file: eyeFile },
-      with: { category: "Hat", file: "Pink Shystie.png" },
-      message: "Glasses and specs do not work with ski masks."
-    }
-  ]),
-  ...[
-    "Black Shystie.png",
-    "Pink Shystie.png"
-  ].flatMap((hatFile) => [
-    {
-      type: "incompatible",
-      trait: { category: "Hat", file: hatFile },
-      with: { category: "Accessories", file: "Bandaid.png" },
-      message: "Bandaid does not work with ski masks."
-    },
-    {
-      type: "incompatible",
-      trait: { category: "Hat", file: hatFile },
-      with: { category: "Mouth", file: "Displeased toothpick.png" },
-      message: "Toothpick does not work with ski masks."
-    },
-    {
-      type: "incompatible",
-      trait: { category: "Hat", file: hatFile },
-      with: { category: "Mouth", file: "Cigar Mouth.png" },
-      message: "Cigar does not work with ski masks."
-    },
-    {
-      type: "incompatible",
-      trait: { category: "Hat", file: hatFile },
-      with: { category: "Mouth", file: "Meh Cigarette.png" },
-      message: "Cigarette does not work with ski masks."
-    },
-    {
-      type: "incompatible",
-      trait: { category: "Hat", file: hatFile },
-      with: { category: "Mouth", file: "Gold Bar.png" },
-      message: "Gold bar does not work with ski masks."
-    }
-  ]),
-  ...[
-    "Bandana Black.png",
-    "Bandana Pink.png"
-  ].flatMap((mouthFile) => [
-    {
-      type: "incompatible",
-      trait: { category: "Hat", file: "BOB Mask.png" },
-      with: { category: "Mouth", file: mouthFile },
-      message: "BOB Mask does not work with bandannas."
-    },
-    {
-      type: "incompatible",
-      trait: { category: "Accessories", file: "BOB-chain.png" },
-      with: { category: "Mouth", file: mouthFile },
-      message: "BOB Chain does not work with bandannas."
-    }
-  ]),
+  ...incompatibleWithMany({
+    category: "Eyes",
+    files: LASER_EYES,
+    withCategory: "Hat",
+    withFiles: FACE_MASKS,
+    message: "Laser eyes do not work with face masks."
+  }),
+  ...incompatibleWithMany({
+    category: "Eyes",
+    files: EYEWEAR,
+    withCategory: "Hat",
+    withFiles: FACE_MASKS,
+    message: "Glasses and specs do not work with face masks."
+  }),
+  ...incompatibleWithMany({
+    category: "Accessories",
+    files: ["Bandaid.png"],
+    withCategory: "Hat",
+    withFiles: FACE_MASKS,
+    message: "Bandaid does not work with face masks."
+  }),
+  ...incompatibleWithMany({
+    category: "Mouth",
+    files: FACE_COVERING_MOUTHS,
+    withCategory: "Hat",
+    withFiles: FACE_MASKS,
+    message: "Bandannas do not work with face masks."
+  }),
+  ...incompatibleWithMany({
+    category: "Mouth",
+    files: PROTRUDING_MOUTH_PROPS,
+    withCategory: "Hat",
+    withFiles: FACE_MASKS,
+    message: "This mouth prop does not work with face masks."
+  }),
+  ...incompatibleWithMany({
+    category: "Accessories",
+    files: ["BOB-chain.png"],
+    withCategory: "Mouth",
+    withFiles: FACE_COVERING_MOUTHS,
+    message: "BOB Chain does not work with bandannas."
+  }),
   {
     type: "onlyWith",
     trait: { category: "Accessories", file: "BOB-chain.png" },
     allowedEmptyCategories: ["Clothes"],
-    allowedWith: [
-      { category: "Clothes", file: "Baby Blue Tee.png" },
-      { category: "Clothes", file: "Black N White Tee.png" },
-      { category: "Clothes", file: "Black N White Tee 2.png" },
-      { category: "Clothes", file: "Black Tee.png" },
-      { category: "Clothes", file: "Black Tee 2.png" },
-      { category: "Clothes", file: "Grey Tee.png" },
-      { category: "Clothes", file: "Kuru Tee.png" },
-      { category: "Clothes", file: "Torn Black Tee.png" },
-      { category: "Clothes", file: "Torn Tee.png" },
-      { category: "Clothes", file: "Torn Tee 2.png" },
-      { category: "Clothes", file: "White Tee.png" },
-      { category: "Clothes", file: "White Tee 2.png" }
-    ],
+    allowedWith: TEE_SHIRTS.map((file) => ({ category: "Clothes", file })),
     message: "BOB Chain only works with tee shirts or no clothing."
   }
 ];
